af0633b6947b1b3f42821bd40bf0432c74de20a0
snpark <atimangel@google.com>
2021-12-26 16:29:48 +0900

    19ing

--- test_21_bonus_wildcard.sh
+++ test_21_bonus_wildcard.sh
@@ -2,6 +2,8 @@
 source ./test.sh
 
 pt "WildCard"
-test 'echo *'
-test 'echo .*'
-test 'echo */'
+test 'echo * | sort'
+test 'echo .* | sort'
+test 'echo */ | sort'
+test 'touch a aa && cat < a* || rm a aa'
+test 'echo hello > oneandonly && cat < one*only && rm oneandonly'
--- test_22_surprise.sh
+++ test_22_surprise.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 source ./test.sh
+
+pt "surprise"
+echo "직접 테스트 해야합니다"
+echo
+echo "echo" '"'"'"'$USER''"'"'"
+echo "echo" "'"'"''$USER'"'"'"'
