d774e54815715346ce6b6edea3f4596035920bf2
snpark <atimangel@google.com>
2021-12-26 19:54:19 +0900

    update

--- test_22_surprise.sh
+++ test_22_surprise.sh
@@ -4,5 +4,7 @@ source ./test.sh
 pt "surprise"
 echo "직접 테스트 해야합니다"
 echo
-echo "echo" '"'"'"'$USER''"'"'"
-echo "echo" "'"'"''$USER'"'"'"'
+echo "echo" '"'"'"'$USER'"'"'"'
+echo "echo" "'"'"''$USER''"'"'"
+echo "echo" "'"'"'"'"'$USER'"'"'"'"'"
+echo "echo" '"'"'"'"''$USER''"'"'"'"'
