6cc70936cb524820e6189a885b1977de52e25b8e
minjakim <76225727+minjakim@users.noreply.github.com>
2021-12-27 13:32:32 +0900

    Update test_13_pwd.sh

--- test_13_pwd.sh
+++ test_13_pwd.sh
@@ -2,3 +2,10 @@
 source ./test.sh
 
 pt "pwd"
+test 'pwd'
+test 'pwd && cd .. && pwd'
+test 'pwd && cd .. && pwd && cd .. && pwd && cd .. && pwd && cd .. && pwd && cd .. && pwd && cd .. && pwd && cd .. && pwd && cd .. && pwd'
+test 'pwd && cd && pwd && cd - && pwd'
+test 'pwd nominihello'
+test 'pwd 1 2 3 4 5 6 7 8 9 0'
+test 'pwd ! @ # $ % ^ & * ( ) _ +'
