25b2dbf8157d907560a90fecba7411d7c0a0e215
minjakim <76225727+minjakim@users.noreply.github.com>
2021-12-27 13:40:07 +0900

    Update test_12_cd.sh

--- test_12_cd.sh
+++ test_12_cd.sh
@@ -2,13 +2,12 @@
 source ./test.sh
 
 pt "cd"
-test 'cd . && pwd '
-test 'cd .. && pwd '
-test 'cd / && pwd '
-test 'cd - && pwd  '
-test 'cd asd && pwd'
-test 'cd qwe && pwd'
-test 'unset OLDPWD && cd - && pwd'
-test 'unset HOME && cd'
-test 'unset PWD && cd .'
-test 'unset OLDPWD && cd .. && cd -'
+test '/bin/pwd && cd && /bin/pwd'
+test 'cd .. && cd -'
+test 'cd .. && cd - && cd - && cd - && cd - && cd - && cd - && cd - && cd - && cd - && cd - && cd - && cd - && cd - && cd - && cd - && cd - && cd - && cd - && cd -'
+test '/bin/pwd && cd / && /bin/pwd'
+test 'cd nominihello'
+test '/bin/pwd && cd nominihello || /bin/pwd'
+test '/bin/pwd && unset OLDPWD && cd - || /bin/pwd'
+test '/bin/pwd && unset HOME && cd || /bin/pwd'
+test '/bin/pwd && unset OLDPWD && cd .. && cd - && /bin/pwd'
