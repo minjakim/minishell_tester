5b30a5c1dfed08c62a3bd653420ceddbfad9417d
snpark <atimangel@google.com>
2021-12-27 09:14:11 +0900

    fix

--- test_16_redirection.sh
+++ test_16_redirection.sh
@@ -15,6 +15,7 @@ test 'echo hi >> hello && echo hello >> hello && cat hello && rm hello'
 test 'echo hello > 1 > 2 > 3 > 4 > 5 && cat 1 2 3 4 5 && rm 1 2 3 4 5'
 test 'echo hello >> 1 > 2 >> 3 > 4 >> 5 && cat 1 2 3 4 5 && rm 1 2 3 4 5'
 test 'echo 1 > 1 && echo 2 > 2 && echo 3 > 3 && echo 4 > 4 && cat < 1 < 2 < 3 < 4 && rm 1 2 3 4'
+echo '<< 1 << 2 << 3 << 4 << 5 << 6 << 7 << 8 << 9 << 10 << 11 << 12 << 13 << 14 << 15 << 16 << 17'
 echo "heredoc은 직접 테스트 해야합니다"
 echo 'cat << eof'
 echo '	hello'
@@ -34,7 +35,7 @@ echo '	"''$EOF''"'
 echo '	eof'
 echo '	$EOF'
 echo
-echo 'cat << ''$EOF'
+echo 'cat << '"'"'$EOF'"'"
 echo '	hello'
 echo '	world'
 echo "	'"'$EOF'"'"
@@ -59,9 +60,6 @@ echo
 echo 'echo hello > hi && cat < hi << eof && rm hi'
 echo
 echo '<< 1 << 2 << 3 << 4 << 5 << 6 << 7 << 8 << 9 << 10 << 11 << 12 << 13 << 14 << 15 << 16'
-echo
-echo '<< 1 << 2 << 3 << 4 << 5 << 6 << 7 << 8 << 9 << 10 << 11 << 12 << 13 << 14 << 15 << 16 << 17'
-echo 'echo $?'
 echo
 echo 'touch 17 && << 1 << 2 << 3 << 4 << 5 << 6 << 7 << 8 << 9 << 10 << 11 << 12 << 13 << 14 << 15 << 16 < 17 && rm 17'
 echo
