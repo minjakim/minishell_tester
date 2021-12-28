48e5a7e13bfcea334c6c496ac2cd0f716a55e707
snpark <54225321+atimangel@users.noreply.github.com>
2021-12-28 10:11:21 +0900

    Update test_15_environment_path.sh

--- test_15_environment_path.sh
+++ test_15_environment_path.sh
@@ -5,17 +5,17 @@ pt "15_environment_path"
 test_leaks "unset PATH && ls"
 test_leaks "unset PATH && cat"
 test_leaks "unset PATH && pwd"
-test_leaks "cd etc/path && unset PATH && ft_echo"
-test_leaks 'cd etc/path && unset PATH && export PATH=$PWD && ft_echo'
-test_leaks 'cd etc/path && unset PATH && export PATH=$PWD/1 && ft_echo'
-test_leaks 'cd etc/path && unset PATH && export PATH=$PWD/2 && ft_echo'
-test_leaks 'cd etc/path && unset PATH && export PATH=$PWD/3 && ft_echo'
-test_leaks 'cd etc/path && unset PATH && export PATH=$PWD/4 && ft_echo'
-test_leaks 'cd etc/path && unset PATH && export PATH=$PWD/5 && ft_echo'
-test_leaks 'cd etc/path && unset PATH && export PATH=$PWD/6 && ft_echo'
-test_leaks 'cd etc/path && unset PATH && export PATH=$PWD:$PWD/1 && ft_echo'
-test_leaks 'cd etc/path && unset PATH && export PATH=$PWD/1:$PWD && ft_echo'
-test_leaks 'cd etc/path && unset PATH && export PATH=:$PWD/1:$PWD && ft_echo'
-test_leaks 'cd etc/path && unset PATH && export PATH=: && ft_echo'
-test_leaks 'cd etc/path && unset PATH && export PATH=: && cd 1 && ft_echo'
-test_leaks 'cd etc/path && unset PATH && export PATH=:::::: && ft_echo'
+test_leaks "cd etc/path && unset PATH && ./ft_echo"
+test_leaks 'cd etc/path && unset PATH && export PATH=$PWD && ./ft_echo'
+test_leaks 'cd etc/path && unset PATH && export PATH=$PWD/1 && ./ft_echo'
+test_leaks 'cd etc/path && unset PATH && export PATH=$PWD/2 && ./ft_echo'
+test_leaks 'cd etc/path && unset PATH && export PATH=$PWD/3 && ./ft_echo'
+test_leaks 'cd etc/path && unset PATH && export PATH=$PWD/4 && ./ft_echo'
+test_leaks 'cd etc/path && unset PATH && export PATH=$PWD/5 && ./ft_echo'
+test_leaks 'cd etc/path && unset PATH && export PATH=$PWD/6 && ./ft_echo'
+test_leaks 'cd etc/path && unset PATH && export PATH=$PWD:$PWD/1 && ./ft_echo'
+test_leaks 'cd etc/path && unset PATH && export PATH=$PWD/1:$PWD && ./ft_echo'
+test_leaks 'cd etc/path && unset PATH && export PATH=:$PWD/1:$PWD && ./ft_echo'
+test_leaks 'cd etc/path && unset PATH && export PATH=: && ./ft_echo'
+test_leaks 'cd etc/path && unset PATH && export PATH=: && cd 1 && ./ft_echo'
+test_leaks 'cd etc/path && unset PATH && export PATH=:::::: && ./ft_echo'
